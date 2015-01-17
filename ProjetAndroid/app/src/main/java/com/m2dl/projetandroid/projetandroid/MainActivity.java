6f2de96f2b7445522894916102f659b7d313e8f2
Assyl Louahadj <assyl.louahadj@gmail.com>
2015-01-17 18:43:18 +0100

    Mise en place de la page d'inscription :D

--- ProjetAndroid/app/src/main/java/com/m2dl/projetandroid/projetandroid/MainActivity.java
+++ ProjetAndroid/app/src/main/java/com/m2dl/projetandroid/projetandroid/MainActivity.java
@@ -2,8 +2,13 @@ package com.m2dl.projetandroid.projetandroid;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,7 +16,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.accueil);
+        setContentView(R.layout.registrationlayout);
     }
 
 
@@ -35,5 +40,38 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public final static boolean isValidEmail(CharSequence target) {
+        return !TextUtils.isEmpty(target) && android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+    }
+
+    public void register(View v)
+    {
+        EditText fullNameET = (EditText) findViewById(R.id.reg_fullname);
+        EditText emailET = (EditText) findViewById(R.id.reg_email);
+
+
+        if (fullNameET.getText().toString().matches(""))
+        {
+            Toast toast= Toast.makeText(getApplicationContext(), "\"Name\" field can't be empty!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+            View view = toast.getView();
+            view.setBackgroundResource(android.R.color.holo_red_light);
+            toast.show();
+            return;
+        }
+
+        if(!isValidEmail(emailET.getText().toString()))
+        {
+            Toast toast= Toast.makeText(getApplicationContext(), "The e-mail adress you entered is incorrect!", Toast.LENGTH_LONG);
+            toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER_HORIZONTAL, 0, 0);
+            View view = toast.getView();
+            view.setBackgroundResource(android.R.color.holo_red_light);
+            toast.show();
+            return;
+        }
+
+        setContentView(R.layout.accueil);
     }
 }
